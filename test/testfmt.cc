bf4605e6bf4a5242286caba95d3c6c6ae57d7a28
ahuston-0 <aliceghuston@gmail.com>
2021-07-21 03:20:16 -0400

    Add more types and fmt::print to testfmt
    This commit adds in tests for several different data types using
    fmt::format_to and sprintf. Each of these tests is timed using chrono's
    steady_clock, a monotonic clock with nanosecond precision. With this, we
    are able to determine that when compiled in debug mode (meaning that
    fmt is added as a shared library), the runtime for the formatting is
    about 1000 nanoseconds with fmt::format_towhile sprintf is about 200
    nanoseconds. In release mode CMake's standard release modeflags (I
    believe this includes -O3), both run in about 180 nanoseconds. These
    averaages are both calculated on an Intel Core i7-8750H CPU (12 threads
    @ 2.20GHz base, 4.10 GHz turbo) after one million formatting iterations.

--- test/testfmt.cc
+++ test/testfmt.cc
@@ -1,12 +1,55 @@
+#include <chrono>
 #include <iostream>
 
+#include "fmt/color.h"
 #include "fmt/core.h"
+#include "fmt/format.h"
 
 using namespace std;
 using namespace fmt;
+using namespace chrono;
+
+template <typename T>
+void test_format(T fmt_val) {
+  print(fg(color::steel_blue) | emphasis::bold, "Currently processing: {}\n",
+        fmt_val);
+  uint64_t sum_time = 0;
+  time_point<steady_clock, nanoseconds> tp;
+  memory_buffer out;
+  char* data = new char[30];
+  uint32_t size;
+  constexpr auto iter = 1000000;
+
+  for (int i = 0; i < iter; i++) {
+    tp = time_point_cast<nanoseconds>(steady_clock::now());
+    out = memory_buffer();
+    format_to(back_inserter(out), "For a moment, {} happened. {}", "nothing",
+              28);
+    data = out.data();  // pointer to the formatted data
+    size = out.size();  // size of the formatted data
+    sum_time += (steady_clock::now() - tp).count();
+  }
+
+  double avg = sum_time * 1.0 / iter;
+  print("fmtlib/fmt: {}\n", avg);
+
+  sum_time = 0;
+  for (int i = 0; i < iter; i++) {
+    tp = time_point_cast<nanoseconds>(steady_clock::now());
+    sprintf(data, "For a moment, %s happened. %d", "nothing", 28);
+    size = strlen(data);
+    sum_time += (steady_clock::now() - tp).count();
+  }
+  avg = sum_time * 1.0 / iter;
+  print("sprintf: {}\n\n", avg);
+}
 
 int main() {
-  auto out = memory_buffer();
-  format_to(back_inserter(out), "Hello, {}", "World!");
-  cout << out.data() << " " << out.size();
+  test_format(1.0f);
+  auto a = 11;
+  test_format(11);
+  test_format(-134135);
+  test_format(135567.362626);
+  test_format(string("hello test").c_str());
+  test_format(string("irjaoijofijfr"));
 }
