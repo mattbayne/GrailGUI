e4fb09eae4f9fd68c0914cf09e4415b525775f0c
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-11-12 12:54:32 -0500

    update curve test and functions

--- src/CAD/Curve.hh
+++ src/CAD/Curve.hh
@@ -1,12 +1,13 @@
 #pragma once
 #include "opengl/GrailGUI.hh"
 #include "opengl/util/Transformation.hh"
+//#include "opengl/MultiShape3D.hh"
 #include "CAD/Vec3d.hh"
 
 /**
 Represent -- 
 **/
-class Curve {
+class Curve  {
   protected:
     std::vector<Vec3D> points;
   
@@ -16,10 +17,22 @@ class Curve {
     }
 
   void add(const Vec3D& p);
-  void resize();
-  uint32_t size() const {return points.size();}
-  virtual Vec3D getPoint(double step) = 0;
-  virtual Vec3D getTangent(double step) = 0; //virtual replaces "abstract" from java
+  void resize() {return points.reserve(points.size()*2);} //.size or .capacity()
+  uint32_t size() const {return points.size();} //size = number of itmes
+  uint32_t capacity() const {return points.capacity();} //capacity = all spaces(used or unused)
+ // virtual Vec3D getPoint(double step) = 0;
+ // virtual Vec3D getTangent(double step) = 0; //virtual replaces "abstract" from java
+  
+  // Overload the << operator
+  friend ostream& operator<< (ostream& s, const Curve& c){
+    for (int i = 0; i < c.points.size(); i++){
+      s << c.points[i] << ",";
+    }
+    return s;
+  }
+  
+  
+  
   //TODO: Transformation getMatrix(double step); //implement Transformation/import for now but function/constructor is different
 };
 
--- test/CurveTest.cc
+++ test/CurveTest.cc
@@ -6,12 +6,17 @@ using namespace std;
 
 int main() {
  // Curve test();
-  Curve test = new Curve();
+  Curve test;
   cout << test << endl;
-  Vec3D p1 = new Vec3D(1,2,3)
+  Vec3D p1(1,2,3);
+  Vec3D p2(4,5,6);
+  //Vec3D p3(7,8,9);
   test.add(p1);
+  test.add(p2);
+  //test.add(p3);
   cout << test.size() << endl;
   test.resize();
+  cout << test.capacity() << endl;
   cout << test << endl;
 }
 
